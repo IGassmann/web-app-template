541db365ead36de7e709954d831b57b4acf8119e
Igor Gassmann <igor@igassmann.me>
2023-01-31 09:40:26 +0200

    config(sentry): remove unnecessary configuration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -9,14 +9,6 @@ Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 1.0,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
