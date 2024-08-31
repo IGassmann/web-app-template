00bc2b0e8a5b884c6f186a21f58eeeb3ffda0e2e
Igor Gassmann <igor@igassmann.me>
2024-08-31 15:37:35 +0200

    chore(deps): upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -4,53 +4,15 @@ const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-
-  // Optional build-time configuration for Sentry.
-  // See https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#extend-nextjs-configuration
-  sentry: {
-    // Tunnel sentry events to help circumvent ad-blockers.
-    tunnelRoute: '/api/sentry',
-  },
-  webpack(config, { webpack }) {
-    // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
-    // into React components. See https://react-svgr.com/docs/next/
-
-    // Grab the existing rule that handles SVG imports
-    // @ts-ignore - this is a private property that is not typed
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
       },
-      // Convert all other *.svg imports to React components
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    // Tree-shake optional Sentry debug code in production builds.
-    // See https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/tree-shaking
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        __SENTRY_DEBUG__: false,
-        __SENTRY_TRACING__: false,
-        __RRWEB_EXCLUDE_IFRAME__: true,
-        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
-        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
-      }),
-    );
-
-    return config;
+    },
   },
 };
 
@@ -66,6 +28,7 @@ const nextConfig = {
  */
 const sentryWebpackPluginOptions = {
   silent: true,
+  tunnelRoute: '/api/sentry', // Tunnel sentry events to help circumvent ad-blockers
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,15 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/']);
 
 export default clerkMiddleware((auth, request) => {
-  if (isPublicRoute(request)) return; // if it's a public route, do nothing
-  auth().protect(); // for any other route, require auth
+  if (isPublicRoute(request)) return;
+  auth().protect();
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/api(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
+    // Always run for API routes, except for the '/api/sentry' route
+    '/api/((?!sentry).*)',
+  ],
 };
