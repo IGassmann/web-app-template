059999725cff7801134909c1792ab2e92a149de3
Igor Gassmann <igor@igassmann.me>
2025-11-03 15:45:35 +0100

    chore: middleware.ts > proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,7 +10,7 @@ export default clerkMiddleware((auth, request) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes, except for the '/api/sentry' route
     '/api/((?!sentry).*)',
   ],
